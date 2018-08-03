740f224e1439aeedd84267004db10dbff8fa0c51
Mohit Tokas <hello@mannanimpex.com>
2018-08-03 23:10:29 +0530

    much changes, such wow

--- js/blog-script.js
+++ js/blog-script.js
@@ -3,13 +3,16 @@ $(document).ready(function () {
     //set equal height and width columns
 
     var colsize = $('.blog_grid > .col-md-6').width();
-
-    $('.blog_grid > .col-md-6').height(colsize);
-    $('.blog_grid > .col-md-6 > .row').height(colsize/2);
+    if(window.innerWidth>576) {
+        $('.blog_grid > .col-md-6').height(colsize);
+        $('.blog_grid > .col-md-6 > .row').height(colsize/2);
+    }
 
     $(window).bind("resize", function () {
-        colsize = $('.blog_grid > .col-md-6').width();
-        $('.blog_grid > .col-md-6').height($(colsize));
-        $('.blog_grid > .col-md-6 > .row').height(colsize/2);
+        if(this.window.innerWidth) {
+            colsize = $('.blog_grid > .col-md-6').width();
+            $('.blog_grid > .col-md-6').height($(colsize));
+            $('.blog_grid > .col-md-6 > .row').height(colsize/2);
+        }
     })
 })
